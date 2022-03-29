547a51a0d7464062bac04b18e5a32bf31b55dad6
AtakanAygun2 <atakan.aygun@gatemarine.com>
2022-03-29 11:28:26 +0300

    eszamanlı_okuma

--- Python.py
+++ Python.py
@@ -1,3 +1,7 @@
+import time
+import random
+from random import randint
+from random import seed
 from datetime import datetime
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
@@ -12,121 +16,136 @@ bucket = "octopusgüçtestbucket"
 
 client = InfluxDBClient(url="http://65.21.107.165:8086", token=token)
 write_api = client.write_api(write_options=SYNCHRONOUS)
-from random import seed
-from random import randint
-import random
-import time
 
-DEBUG = 1 
+DEBUG = 1
 ON = 1
 OFF = 0
-##Sensor initializations
-#adresler octopus adreslerine göre verilecek
-##GSMli
-ina1 = INA219(0.1, 0.5, address=0x45,busnum=0)
+# Sensor initializations
+# adresler octopus adreslerine göre verilecek
+# GSMli
+ina1 = INA219(0.1, 0.5, address=0x45, busnum=0)
 ina1.configure(ina1.RANGE_16V, ina1.GAIN_AUTO)
-##GPSliGSMsiz
-ina2 = INA219(0.1, 0.5, address=0x45,busnum=0)
+# GPSliGSMsiz
+ina2 = INA219(0.1, 0.5, address=0x45, busnum=0)
 ina2.configure(ina2.RANGE_16V, ina2.GAIN_AUTO)
-##GPSsiz
-ina3 = INA219(0.1, 0.5, address=0x45,busnum=0)
+# GPSsiz
+ina3 = INA219(0.1, 0.5, address=0x45, busnum=0)
 ina3.configure(ina3.RANGE_16V, ina3.GAIN_AUTO)
-##SEALITE
-ina4 = INA219(0.1, 0.5, address=0x45,busnum=0)
+# SEALITE
+ina4 = INA219(0.1, 0.5, address=0x45, busnum=0)
 ina4.configure(ina4.RANGE_16V, ina4.GAIN_AUTO)
 
+
 def get_randomvoltage():
-    return randint(0,10)*0.1 + 12
+    return randint(0, 10)*0.1 + 12
+
+
 def get_randomcurrent_on():
-    return randint(0,5)*0.01 + 0.15
+    return randint(0, 5)*0.01 + 0.15
+
+
 def get_randomcurrent_off():
-    return  randint(0,5)*0.01+0.03
+    return randint(0, 5)*0.01+0.03
+
 
 if(DEBUG == 1):
-    def returnlanternpowerdata(ina219_id,measurement_name,device_name,sel):   
+    def returnlanternpowerdata(ina219_id, measurement_name, device_name, sel):
         if(sel == 1):
             random_voltage = get_randomvoltage()
             random_current = get_randomcurrent_on()
             random_power = random_voltage*random_current
-            power_data ={
+            power_data = {
                 "measurement": measurement_name,
                 "tags": {
-                "Device": device_name,
-                                 },
+                    "Device": device_name,
+                },
                 "fields": {
-                "bus_voltage": random_voltage ,
-                "current":  random_current,
-                "power": random_power
-                    }
+                    "bus_voltage": random_voltage,
+                    "current":  random_current,
+                    "power": random_power
                 }
+            }
         else:
             random_voltage = get_randomvoltage()
             random_current = get_randomcurrent_off()
             random_power = random_voltage*random_current
-            power_data ={
-            "measurement": measurement_name,
-            "tags": {
-            "Device": device_name,
-                                 },
-            "fields": {
-            "bus_voltage": random_voltage,
-            "current":  random_current,
-            "power": random_power
-                 }
-            } 
-                
-        return power_data
-else:
-    def returnlanternpowerdata(ina219_id,measurement_name,device_name):   
-        
-        power_data ={
+            power_data = {
                 "measurement": measurement_name,
                 "tags": {
-                "Device": device_name,
-                                 },
+                    "Device": device_name,
+                },
                 "fields": {
-                "bus_voltage": ina219_id.voltage() ,
+                    "bus_voltage": random_voltage,
+                    "current":  random_current,
+                    "power": random_power
+                }
+            }
+
+        return power_data
+else:
+    def returnlanternpowerdata(ina219_id, measurement_name, device_name):
+
+        power_data = {
+            "measurement": measurement_name,
+            "tags": {
+                "Device": device_name,
+            },
+            "fields": {
+                "bus_voltage": ina219_id.voltage(),
                 "current":  ina219_id.current(),
                 "power": ina219_id.power()
-                    }
-                }
+            }
+        }
         return power_data
-
 
 
 def application_start_powertest():
-    measurement_period=0.1
+    measurement_period = 0.1
     if(DEBUG == 1):
         for _ in range(20):
+            data1 = returnlanternpowerdata(ina1, "power_data", "GSMLANTERN", ON)
+            data2 = returnlanternpowerdata(ina2, "power_data", "GPSLANTERN", ON)
+            data3 = returnlanternpowerdata(ina3, "power_data", "GPSSİZLANTERN", ON)
+            data4 = returnlanternpowerdata(ina4, "power_data", "SEALİTELANTERN", ON)
             time.sleep(measurement_period)
-            write_api.write(bucket, org,returnlanternpowerdata(ina1,"power_data","GSMLANTERN",ON))
+            write_api.write(bucket, org, data1)
             time.sleep(0.001)
-            write_api.write(bucket, org,returnlanternpowerdata(ina2,"power_data","GPSLANTERN",ON))
+            write_api.write(bucket, org, data2)
             time.sleep(0.001)
-            write_api.write(bucket, org,returnlanternpowerdata(ina3,"power_data","GPSSİZLANTERN",ON))
+            write_api.write(bucket, org, data3)
             time.sleep(0.001)
-            write_api.write(bucket, org,returnlanternpowerdata(ina4,"power_data","SEALİTELANTERN",ON))
-                    
+            write_api.write(bucket, org, data4)
+
         for _ in range(60):
-            
+
+            data1 = returnlanternpowerdata(ina1, "power_data", "GSMLANTERN", ON)
+            data2 = returnlanternpowerdata(ina2, "power_data", "GPSLANTERN", ON)
+            data3 = returnlanternpowerdata(ina3, "power_data", "GPSSİZLANTERN", ON)
+            data4 = returnlanternpowerdata(ina4, "power_data", "SEALİTELANTERN", ON)
             time.sleep(measurement_period)
-            write_api.write(bucket, org,returnlanternpowerdata(ina1,"power_data","GSMLANTERN",OFF))
+            write_api.write(bucket, org, data1)
             time.sleep(0.001)
-            write_api.write(bucket, org,returnlanternpowerdata(ina2,"power_data","GPSLANTERN",OFF))
+            write_api.write(bucket, org, data2)
             time.sleep(0.001)
-            write_api.write(bucket, org,returnlanternpowerdata(ina3,"power_data","GPSSİZLANTERN",OFF))
+            write_api.write(bucket, org, data3)
             time.sleep(0.001)
-            write_api.write(bucket, org,returnlanternpowerdata(ina4,"power_data","SEALİTELANTERN",OFF))
+            write_api.write(bucket, org, data4)
 
     else:
+        data1 = returnlanternpowerdata(ina1, "power_data", "GSMLANTERN", ON)
+        data2 = returnlanternpowerdata(ina2, "power_data", "GPSLANTERN", ON)
+        data3 = returnlanternpowerdata(ina3, "power_data", "GPSSİZLANTERN", ON)
+        data4 = returnlanternpowerdata(ina4, "power_data", "SEALİTELANTERN", ON)
         time.sleep(measurement_period)
-        write_api.write(bucket, org,returnlanternpowerdata(ina1,"power_data","GSMLANTERN"))
+        write_api.write(bucket, org, data1)
         time.sleep(0.001)
-        write_api.write(bucket, org,returnlanternpowerdata(ina2,"power_data","GPSLANTERN"))
+        write_api.write(bucket, org, data2)
         time.sleep(0.001)
-        write_api.write(bucket, org,returnlanternpowerdata(ina3,"power_data","GPSSİZLANTERN"))
+        write_api.write(bucket, org, data3)
         time.sleep(0.001)
-        write_api.write(bucket, org,returnlanternpowerdata(ina4,"power_data","SEALİTELANTERN"))
+        write_api.write(bucket, org, data4)
+
+
 while(1):
     application_start_powertest()
 
